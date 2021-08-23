7f03efb7a5f4f1e00e1e227087c349903b58c1af
kritikas726 <kritikas726@gmail.com>
2021-08-23 21:22:19 +0530

    defined theme for styled components and created a few style components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './Pages.js/Home';
 import Show from './Pages.js/Show';
 import Starred from './Pages.js/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/starred" exact>
-        <Starred />
-      </Route>
-      <Route path="/show/:id">
-        <Show />
-      </Route>
-      <Route>This is 404 page.</Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/starred" exact>
+          <Starred />
+        </Route>
+        <Route path="/show/:id">
+          <Show />
+        </Route>
+        <Route>This is 404 page.</Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Navs.js
+++ src/Components/Navs.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import { NavList, LinkStyled } from './Navs.styled';
 
 const Navs = () => {
   const LINKS = [
@@ -12,15 +13,24 @@ const Navs = () => {
       text: 'Starred',
     },
   ];
+
+  const location = useLocation();
+  console.log('location', location);
+
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map(item => (
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
